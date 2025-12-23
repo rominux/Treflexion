4bb115eaef72e4569bde3af154c80c2cc57f0165
rominux <romain.59.lefebvre@gmail.com>
2025-12-23 23:25:31 +0100

    Add files via upload

---
+++ java/Question.java
@@ -0,0 +1,9 @@
+class Question {
+    String enonce;
+    String repA;
+    String repB;
+    String repC;
+    String repD;
+    String bonneReponse; // "A", "B", "C" ou "D"
+    String explication;
+}
