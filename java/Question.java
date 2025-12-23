c57c51dc346840382ca4ed5cbdd5e0e30c25cab8
rominux <romain.59.lefebvre@gmail.com>
2025-12-23 23:46:55 +0100

    Delete java directory

--- java/Question.java
+++
@@ -1,9 +0,0 @@
-class Question {
-    String enonce;
-    String repA;
-    String repB;
-    String repC;
-    String repD;
-    String bonneReponse; // "A", "B", "C" ou "D"
-    String explication;
-}
